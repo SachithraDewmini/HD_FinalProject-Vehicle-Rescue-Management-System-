8acaf07bdc6f714eec253df906981e5ddb3f4b1a
Sachithra Dewmini Panditha <sdpanditha123@gmail.com>
2024-12-01 12:35:56 +0530

    commit

--- Fix_and_Go_web/FixAndGoWeb/src/components/Buttons/Mechanic.jsx
+++ Fix_and_Go_web/FixAndGoWeb/src/components/Buttons/Mechanic.jsx
@@ -18,9 +18,15 @@ const Mechanic = () => {
 
       {/* Content Section */}
       <div className='container text-center md:text-left mb-8'>
-        <p className='font-semibold'>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio voluptas ipsa commodi, dignissimos iste dicta molestiae blanditiis eaque voluptatibus modi maiores ad quis. Iste totam eos excepturi in expedita vitae.
+      <p className='font-semibold mb-8'>
+        Vehicle Repair and Maintenance – Fixing engine issues, brake problems, and general mechanical repairs.
         </p>
+        <p className='font-semibold mb-8'>On-Location Repairs – Providing minor repairs at the breakdown site</p>
+        <p className='font-semibold mb-8'>Spare Parts Installation – Supplying and installing replacement parts.</p>
+        <p className='font-semibold mb-8'>Diagnostic Services – Identifying problems using advanced diagnostic tools</p>
+        <p className='font-semibold mb-8'>Emergency Repairs – Quick fixes during urgent situations.</p>
+        <p className='font-semibold mb-8'>Customer Messaging – Direct communication with customers through the system</p>
+        <p className='font-semibold mb-8'>Feedback Analysis – Reviewing customer ratings to improve service quality</p>
         <div className='flex gap-2 items-center md:justify-start justify-center'>
           <button className='primary-btn'>Download App</button>
         </div>
