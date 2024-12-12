bd7d159ebf7a053328a0751e1fb7a9f23e12d8d4
Sachithra Dewmini Panditha <sdpanditha123@gmail.com>
2024-12-12 15:08:18 +0530

    commit

--- Fix_and_Go_web/FixAndGoWeb/src/components/Buttons/Customer.jsx
+++ Fix_and_Go_web/FixAndGoWeb/src/components/Buttons/Customer.jsx
@@ -18,13 +18,13 @@ const Customer = () => {
 
       {/* Content Section */}
       <div className='container text-center md:text-left my-8'>
-       <p className='font-semibold mb-8'>Vehicle Repair and Maintenance – Fixing engine issues, brake problems, and general mechanical repairs.</p>
-        <p className='font-semibold mb-8'>On-Location Repairs – Providing minor repairs at the breakdown site</p>
-        <p className='font-semibold mb-8'>Spare Parts Installation – Supplying and installing replacement parts.</p>
-        <p className='font-semibold mb-8'>Diagnostic Services – Identifying problems using advanced diagnostic tools</p>
-        <p className='font-semibold mb-8'>Emergency Repairs – Quick fixes during urgent situations.</p>
-        <p className='font-semibold mb-8'>Customer Messaging – Direct communication with customers through the system</p>
-        <p className='font-semibold mb-8'>Feedback Analysis – Reviewing customer ratings to improve service quality</p>
+       <p className='font-semibold mb-8'>-  Vehicle Repair and Maintenance – Fixing engine issues, brake problems, and general mechanical repairs.</p>
+        <p className='font-semibold mb-8'>-  On-Location Repairs – Providing minor repairs at the breakdown site</p>
+        <p className='font-semibold mb-8'>-  Spare Parts Installation – Supplying and installing replacement parts.</p>
+        <p className='font-semibold mb-8'>-  Diagnostic Services – Identifying problems using advanced diagnostic tools</p>
+        <p className='font-semibold mb-8'>-  Emergency Repairs – Quick fixes during urgent situations.</p>
+        <p className='font-semibold mb-8'>-  Customer Messaging – Direct communication with customers through the system</p>
+        <p className='font-semibold mb-8'>-  Feedback Analysis – Reviewing customer ratings to improve service quality</p>
         <div className='flex gap-2 items-center md:justify-start justify-center'>
           <button className='primary-btn'>Download App</button>
         </div>
