33b6e6d72dfc60469cd38c5e3b7191d54c03e8fe
Sachithra Dewmini Panditha <sdpanditha123@gmail.com>
2024-12-17 13:17:36 +0530

    add code

--- Fix_and_Go_web/FixAndGoWeb/src/App.jsx
+++ Fix_and_Go_web/FixAndGoWeb/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Admindashbord from './Admin/Admindashbord';
+import Admindashbord from './Admindashbord';
 import About from './components/About/About';
 import Customer from './components/Buttons/Customer';
 import Mechanic from './components/Buttons/Mechanic';
@@ -8,9 +8,22 @@ import RentalOwner from './components/Buttons/RentalOwner';
 import Troller from './components/Buttons/Troller';
 import Contact from './components/Contact/Contact';
 import Home from './components/Home/Home';
-import Login from './components/Login/Login';
 import Navbar from './components/Navbar/Navbar';
 import Services from './components/Services/Services';
+import CusMechanicView from './CusMechanicView';
+import CustomerView from './CustomerView';
+import Login from './Login';
+import MechanicAdd from './MechanicAdd';
+import MechanicEdit from './MechanicEdit';
+import MechanicView from './MechanicView';
+import RentalOwnerAdd from './RentalOwnerAdd';
+import RentalOwnerEdit from './RentalOwnerEdit';
+import RentalOwnerView from './RentalOwnerView';
+import TrollerAdd from './TrollerAdd';
+import TrollerEdit from './TrollerEdit';
+import TrollerView from './TrollerView';
+import UserLogin from './UserLogin';
+import UserRegister from './UserRegister';
 
 const App = () => {
   return (
@@ -28,8 +41,19 @@ const App = () => {
         <Route path="/mechanic" element={<><Mechanic /><Contact /></>} />
         <Route path="/rentalowner" element={<><RentalOwner /><Contact /></>} />
         <Route path="/admindash" element={<Admindashbord />} />
- 
-        
+        <Route path="/userRegiser" element={<UserRegister />} />
+        <Route path="/userLogin" element={<UserLogin />} />
+        <Route path="/mview" element={<MechanicView />} />
+        <Route path="/rview" element={<RentalOwnerView />} />
+        <Route path="/tview" element={<TrollerView />} />
+        <Route path="/cview" element={<CustomerView />} />
+        <Route path="/madd" element={<MechanicAdd />} />
+        <Route path="/radd" element={<RentalOwnerAdd />} />
+        <Route path="/tadd" element={<TrollerAdd />} />
+        <Route path="/medit" element={<MechanicEdit />} />
+        <Route path="/redit" element={<RentalOwnerEdit />} />
+        <Route path="/tedit" element={<TrollerEdit />} />
+        <Route path="/cmview" element={<CusMechanicView />} />
       </Routes>
     </Router>
   );
